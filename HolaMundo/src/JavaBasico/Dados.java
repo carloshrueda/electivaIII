45b8d62b960e98c8b175c94bf33bee6bf24a0324
Carlos Rueda <carloshrueda@gmail.com>
2017-08-24 18:31:13 -0500

    Modificación de la clase Dados

--- HolaMundo/src/JavaBasico/Dados.java
+++ HolaMundo/src/JavaBasico/Dados.java
@@ -18,33 +18,33 @@ public class Dados {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int i,d1,d2,sum;
-        SecureRandom r= new SecureRandom();
-        int[] vfrec= new int[13];
-        
-        for(i=1; i<1000000; ++i) {
-            d1= r.nextInt(6)+1;
-            d2= r.nextInt(6)+1;
-            sum= d1+d2;
-            ++vfrec[sum] ;
+        int i, d1, d2, sum;
+        int[] vfrec = new int[11];
+        final int veces = 1000000;
+
+        SecureRandom r = new SecureRandom();
+        //sembrando el generador de aleatorios
+        r.setSeed(System.currentTimeMillis());
+        for (i = 0; i < veces; ++i) {
+            d1 = r.nextInt(6);
+            d2 = r.nextInt(6);
+            sum = d1 + d2;
+            ++vfrec[sum];
         }
-        
-        int max= vfrec[2];
-        int imax= 2;
-        System.out.printf("%s%12s%n", "Valor", "Frecuencia");
-        for(i=2; i<vfrec.length; i++) {
-            System.out.printf("%5d%10d%20F%n", i, vfrec[i], float(float(vfrec[i])/1000000.0));
-            if (vfrec[i]>max) {
-                max= vfrec[i];
-                imax= i;
+
+        int max = vfrec[0];
+        int imax = 0;
+        System.out.printf("%s%12s%5s%n", "Valor", "Frecuencia", "%");
+        for (i = 0; i < vfrec.length; i++) {
+            System.out.printf("%5d%10d%10.3f%n", i + 2, vfrec[i], 
+                    ((float) (vfrec[i]) / (float) (veces) * 100.0));
+            if (vfrec[i] > max) {
+                max = vfrec[i];
+                imax = i + 2;
             }
         }
-        
-        System.out.println("El valor que mas cae es " + imax);
-        
-        
-        
-        
+
+        System.out.println("\nEl valor que mas cae es: " + imax);
     }
-     
+
 }

ab6d0a7eec7bd7b440e95bcfa41c63c87fe9e47a
Carlos Rueda <carloshrueda@gmail.com>
2017-08-24 18:59:11 -0500

    Aumentando la seguridad en la generación de número aleatórios.

--- HolaMundo/src/JavaBasico/Dados.java
+++ HolaMundo/src/JavaBasico/Dados.java
@@ -5,6 +5,7 @@
  */
 package JavaBasico;
 
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 /**
@@ -18,25 +19,25 @@ public class Dados {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int i, d1, d2, sum;
+        int i;
         int[] vfrec = new int[11];
         final int veces = 1000000;
 
-        SecureRandom r = new SecureRandom();
-        //sembrando el generador de aleatorios
-        r.setSeed(System.currentTimeMillis());
-        for (i = 0; i < veces; ++i) {
-            d1 = r.nextInt(6);
-            d2 = r.nextInt(6);
-            sum = d1 + d2;
-            ++vfrec[sum];
+        SecureRandom r;
+        try {
+            r = SecureRandom.getInstance("SHA1PRNG");
+            for (i = 0; i < veces; ++i) {
+                ++vfrec[r.nextInt(6) + r.nextInt(6)];
+            }
+        } catch (NoSuchAlgorithmException ex) {
+            System.err.println(Dados.class.getName() + "\n" + ex.getMessage());
         }
 
         int max = vfrec[0];
         int imax = 0;
         System.out.printf("%s%12s%5s%n", "Valor", "Frecuencia", "%");
         for (i = 0; i < vfrec.length; i++) {
-            System.out.printf("%5d%10d%10.3f%n", i + 2, vfrec[i], 
+            System.out.printf("%5d%10d%10.3f%n", i + 2, vfrec[i],
                     ((float) (vfrec[i]) / (float) (veces) * 100.0));
             if (vfrec[i] > max) {
                 max = vfrec[i];

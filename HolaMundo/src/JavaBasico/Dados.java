8e152f3ff7cf2476df9d4ed86ef966d33ad09950
Carlos Rueda <carloshrueda@gmail.com>
2017-08-24 20:00:49 -0500

    implementando varias versiones de la clase Dado con varias implementaciones de generadores de numeros aleatorios

--- HolaMundo/src/JavaBasico/Dados.java
+++ HolaMundo/src/JavaBasico/Dados.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package JavaBasico;
 
 import java.security.NoSuchAlgorithmException;
@@ -14,11 +9,7 @@ import java.security.SecureRandom;
  */
 public class Dados {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         int i;
         int[] vfrec = new int[11];
         final int veces = 1000000;
@@ -31,7 +22,7 @@ public class Dados {
                 //Aumentando la frecuencia a medida que caen
                 ++vfrec[r.nextInt(6) + r.nextInt(6)];
             }
-            
+
             //Imprimir resultados
             int max = vfrec[0];
             int imax = 0;
@@ -39,7 +30,7 @@ public class Dados {
             for (i = 0; i < vfrec.length; i++) {
                 System.out.printf("%5d%10d%10.3f%n", i + 2, vfrec[i],
                         ((float) (vfrec[i]) / (float) (veces) * 100.0));
-                
+
                 //Buscando el numero con la frecuencia mayor
                 if (vfrec[i] > max) {
                     max = vfrec[i];

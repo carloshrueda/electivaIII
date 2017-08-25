ea10ff38fac7bc7bd3877800b4abf39bf33ed4d2
Carlos Rueda <carloshrueda@gmail.com>
2017-08-24 19:03:07 -0500

    Documentando el código

--- HolaMundo/src/JavaBasico/Dados.java
+++ HolaMundo/src/JavaBasico/Dados.java
@@ -25,27 +25,33 @@ public class Dados {
 
         SecureRandom r;
         try {
+            //generando los lanzamientos de dados
             r = SecureRandom.getInstance("SHA1PRNG");
             for (i = 0; i < veces; ++i) {
+                //Aumentando la frecuencia a medida que caen
                 ++vfrec[r.nextInt(6) + r.nextInt(6)];
             }
+            
+            //Imprimir resultados
+            int max = vfrec[0];
+            int imax = 0;
+            System.out.printf("%s%12s%5s%n", "Valor", "Frecuencia", "%");
+            for (i = 0; i < vfrec.length; i++) {
+                System.out.printf("%5d%10d%10.3f%n", i + 2, vfrec[i],
+                        ((float) (vfrec[i]) / (float) (veces) * 100.0));
+                
+                //Buscando el numero con la frecuencia mayor
+                if (vfrec[i] > max) {
+                    max = vfrec[i];
+                    imax = i + 2;
+                }
+            }
+
+            System.out.println("\nEl valor que mas cae es: " + imax);
         } catch (NoSuchAlgorithmException ex) {
             System.err.println(Dados.class.getName() + "\n" + ex.getMessage());
         }
 
-        int max = vfrec[0];
-        int imax = 0;
-        System.out.printf("%s%12s%5s%n", "Valor", "Frecuencia", "%");
-        for (i = 0; i < vfrec.length; i++) {
-            System.out.printf("%5d%10d%10.3f%n", i + 2, vfrec[i],
-                    ((float) (vfrec[i]) / (float) (veces) * 100.0));
-            if (vfrec[i] > max) {
-                max = vfrec[i];
-                imax = i + 2;
-            }
-        }
-
-        System.out.println("\nEl valor que mas cae es: " + imax);
     }
 
 }

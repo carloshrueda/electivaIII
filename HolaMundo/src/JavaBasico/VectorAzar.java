5c9b25cab953ecffff27d9ff3890aebe9ff4f10f
usuario <usuario@DOCENTE13>
2017-08-30 10:36:23 -0500

    Se concluyo la clase VectorAzar

--- HolaMundo/src/JavaBasico/VectorAzar.java
+++ HolaMundo/src/JavaBasico/VectorAzar.java
@@ -10,37 +10,44 @@ import java.util.Arrays;
  */
 public class VectorAzar {
 
-    public static void imprimirVec(int[] v, String msg) {
-        System.out.printf("%n%s%n",msg);
-        for(int e:v){
-            System.out.printf("%d, ",e);
+    public static void imprimirVec(int[] v, int i, int f, String msg) {
+        System.out.printf("%n%s%n", msg);
+        for (int k = i; k < f; k++) {
+            System.out.printf("%d, ", v[k]);
         }
         System.out.println();
     }
-    
+
     public static void main(String[] args) {
-        int[] vint = {15,32,25,18,17,1,3,39,37,96,81,4,5,89,12,46,32,89,10,55,36};
+        int[] vint = {15, 32, 25, 18, 17, 1, 3, 39, 37, 96, 81, 4, 5, 89, 12, 46, 32, 89, 10, 55, 36};
 
         int i;
-        final int tam= 7;
-        int[] parcial = new int[tam];
+        final int tam = 7;
+        int[] vfin = new int[tam];
         int p, t;
 
         SecureRandom r;
         try {
             r = SecureRandom.getInstance("SHA1PRNG");
-            imprimirVec(vint, "Vector inicial:");
-            /*for (i = 0; i < tam+1; ++i) {
-                p = r.nextInt(vint.length-i);
-                parcial[i]= vint[p];
-                vint[p]= vint[vint.length-i-1];
-                vint[vint.length-i-1]= parcial[i];
+            imprimirVec(vint, 0, vint.length, "Vector inicial:");
+            System.out.println("Tamaño del vector: " + vint.length);
+            for (i = 0; i < tam; ++i) {
+                imprimirVec(vint, 0, vint.length - i, "VINT:");
+                p = r.nextInt(vint.length - i);
+                System.out.printf("%nP: %d <--> vinit[%d]=%d", p, p, vint[p]);
+                vfin[i] = vint[p];
+                imprimirVec(vfin, 0, i+1, "VFIN:");
+                vint[p] = vint[vint.length - 1 - i];
+                vint[vint.length - 1 - i] = vfin[i];
+                imprimirVec(vint, 0, vint.length, "VINT:");
+                System.out.println("-------------------------------------------");
             }
-            Arrays.sort(parcial);
+            Arrays.sort(vfin);
 
-            for (i = 0; i < parcial.length - 1; i++) {
-                System.out.printf("%d -> %s%n",(i+1),parcial[i]);
-            }*/
+            System.out.println("\nVECTOR FINAL");
+            for (i = 0; i < vfin.length; i++) {
+                System.out.printf("%d -> %s%n",i,vfin[i]);
+            }
 
         } catch (NoSuchAlgorithmException ex) {
             System.err.println(Dados.class.getName() + "\n" + ex.getMessage());

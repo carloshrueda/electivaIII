f4552ebcc0aad22b74b8a84a126bcb1d15e18161
carloshrueda <carloshrueda@gmail.com>
2017-09-06 20:57:46 -0500

    probando desde eclipse

--- Prueba2/src/prueba2/Prueba2.java
+++ Prueba2/src/prueba2/Prueba2.java
@@ -15,7 +15,7 @@ public class Prueba2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola soy prueba2");
+        System.out.println("Hola soy prueba2 ...");
     }
     
 }

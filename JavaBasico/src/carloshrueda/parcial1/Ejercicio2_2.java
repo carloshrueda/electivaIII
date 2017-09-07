770d358c81bed494b01258aaa86c31a3a37017fc
carloshrueda <carloshrueda@gmail.com>
2017-09-06 23:18:40 -0500

    - Se terminó el ejercicio2_2.java con una implementación usando la clase Vector

--- JavaBasico/src/carloshrueda/parcial1/Ejercicio2_2.java
+++ JavaBasico/src/carloshrueda/parcial1/Ejercicio2_2.java
@@ -8,12 +8,13 @@
  * subtotal menos descuentos y el valor total correspondiente al subtotal, menos 
  * los descuentos, mas el iva.
  * 
- * Resolución con vectores - version 3 (ArrayList)
+ * Resolución con vectores - version 3 (Clase Vector)
  */
 package carloshrueda.parcial1;
 
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.Vector;
+import java.util.Collections;
 
 /**
  *
@@ -21,69 +22,72 @@ import java.util.Arrays;
  */
 public class Ejercicio2_2 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        int cant, i;
-        long precio, subtotal, descuento, iva, total;
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		int cant, i;
+		int precio, subtotal, descuento, iva, total;
 
-        //Ingresar valores
-        Scanner scn = new Scanner(System.in);
-        System.out.print("(ejercicio 2_1) Cuantos articulos son?: ");
-        cant = scn.nextInt();
-        while (cant < 1) {
-            System.out.println("Error. Cantidad de artículos inválida. (n>0)");
-            System.out.println("\nCuantos artículos son?: ");
-            cant = scn.nextInt();
-        }
+		Vector<Integer> vprodprecio = new Vector<Integer>();
 
-        long[] vprodprecio = new long[cant];
+		// Ingresar valores
+		Scanner scn = new Scanner(System.in);
+		System.out.print("(ejercicio 2_2) Cuantos articulos son?: ");
+		cant = scn.nextInt();
+		while (cant < 1) {
+			System.out.println("Error. Cantidad de artículos inválida. (n>0)");
+			System.out.println("\nCuantos artículos son?: ");
+			cant = scn.nextInt();
+		}
 
-        subtotal = 0;
-        for (i = 0; i < cant; i++) {
-            System.out.print("Ingrese el precio del producto: ");
-            precio = scn.nextInt();
-            while (precio < 1) {
-                System.out.println("Error. Precio inválido (precio > 0)\n");
 
-                System.out.print("Ingrese el precio del producto: ");
-                precio = scn.nextInt();
-            }
-            vprodprecio[i] = precio;
-            subtotal += precio;
+		subtotal = 0;
+		for (i = 0; i < cant; i++) {
+			System.out.print("Ingrese el precio del producto: ");
+			precio = scn.nextInt();
+			while (precio < 1) {
+				System.out.println("Error. Precio inválido (precio > 0)\n");
 
-        }
-        scn.close();
+				System.out.print("Ingrese el precio del producto: ");
+				precio = scn.nextInt();
+			}
+			vprodprecio.addElement(precio);
+			subtotal += precio;
 
-        //Impresion de resultados (Precios y subtotal)
-        System.out.printf("%n%20s%n", "MENOS X MAS");
-        System.out.println(String.format("%0" + 30 + "d", 0).replace("0", "-"));
-        for (i = 0; i < cant; i++) {
-            System.out.printf("%3s%2d%5s%d%n", "Artículo", (i+1), "$", vprodprecio[i]);
-        }
-        System.out.println(String.format("%0" + 25 + "d", 0).replace("0", "-"));
-        System.out.printf("%9s%6s%d%n", "SUBTOTAL", "$", subtotal);
+		}
+		scn.close();
 
-        //Encontrar los tres productos mas caros
-        Arrays.sort(vprodprecio); //ordena de menor a mayor
+		// Impresion de resultados (Precios y subtotal)
+		System.out.printf("%n%20s%n", "MENOS X MAS");
+		System.out.println(String.format("%0" + 30 + "d", 0).replace("0", "-"));
+		for (i = 0; i < cant; i++) {
+			System.out.printf("%3s%2d%5s%d%n", "Artículo", (i + 1), "$", vprodprecio.get(i));
+		}
+		System.out.println(String.format("%0" + 25 + "d", 0).replace("0", "-"));
+		System.out.printf("%9s%6s%d%n", "SUBTOTAL", "$", subtotal);
 
-        //Calculo de descuentos
-        descuento = 0;
-        for (i = 1; ( (i <= 3) && ((cant - i) >= 0)); i++) {
-            descuento += Math.round((double) vprodprecio[cant - i] * ((double) (4-i) / 10.0));
-        }
+		// Encontrar los tres productos mas caros
+		Collections.sort(vprodprecio, Collections.reverseOrder()); // ordena de mayor a menor
 
-        //Calculo del IVA
-        iva = Math.round(((double) (subtotal - descuento) * 0.2));
-        //Calculo del total
-        total = subtotal - descuento + iva;
+		// Calculo de descuentos
+		System.out.println(vprodprecio.toString());
+		descuento = 0;
+		for (i = 1; ((i <= 3) && (i <= vprodprecio.size())); i++) {
+			descuento += Math.round((double) vprodprecio.get(i - 1) * ((double) (4 - i) / 10.0));
+		}
 
-        System.out.printf("%n%11s%4s%d%n", "DESCUENTOS", "$", Math.round(descuento));
-        System.out.printf("%n%4s%11s%d%n", "IVA", "$", Math.round(iva));
+		// Calculo del IVA
+		iva = (int) Math.round(((double) (subtotal - descuento) * 0.2));
+		// Calculo del total
+		total = subtotal - descuento + iva;
 
-        System.out.println(String.format("%0" + 25 + "d", 0).replace("0", "-"));
-        System.out.printf("%6s%9s%d%n", "TOTAL", "$", Math.round(total));
-        System.out.println(String.format("%0" + 25 + "d", 0).replace("0", "-"));
-    }
+		System.out.printf("%n%11s%4s%d%n", "DESCUENTOS", "$", Math.round(descuento));
+		System.out.printf("%n%4s%11s%d%n", "IVA", "$", Math.round(iva));
+
+		System.out.println(String.format("%0" + 25 + "d", 0).replace("0", "-"));
+		System.out.printf("%6s%9s%d%n", "TOTAL", "$", Math.round(total));
+		System.out.println(String.format("%0" + 25 + "d", 0).replace("0", "-"));
+	}
 }

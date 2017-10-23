6b2697cad172fc9d16ce8cf5a92873e868a7446f
ESTUDIANTE <ESTUDIANTE@S4-2>
2017-10-23 07:25:55 -0500

    Agrego clase ArchivosClase.java Agrego datos.txt al proyecto y a la carpeta docs

--- prArchivos/src/prarchivos/LeerArchivoTexto.java
+++ prArchivos/src/prarchivos/LeerArchivoTexto.java
@@ -1,14 +1,15 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template scn, choose Tools | Templates
  * and open the template in the editor.
  */
 package prarchivos;
 
 import java.util.Scanner;
-import java.io.File;
+//import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.FileReader;
+//import java.io.IOException;
 
 /**
  *
@@ -20,23 +21,27 @@ public class LeerArchivoTexto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String nomarchivo= "dataFile.txt";
+        String nomarchivo= "C:\\Users\\ESTUDIANTE\\Documents\\NetBeansProjects\\electivaIII\\prArchivos\\src\\prarchivos\\dataFile.txt";
         
         try {
-            Scanner file = new Scanner(new File(nomarchivo));
+            Scanner scn = new Scanner(new FileReader(nomarchivo));
             
-            while (file.hasNext()) {
-                String stringRead = file.nextLine();
+            System.out.println("Inicio" + scn.hasNext());
+            
+            while (scn.hasNext()) {
+                String stringRead = scn.nextLine();
                 System.out.println(stringRead);
             }
-            
-            file.close();
+            System.out.println("fin");
+            scn.close();
         } catch (FileNotFoundException ex) {
-            System.err.println("Error. No se encontró el archivo " + nomarchivo);
+            System.out.println("Error. No se encontró el archivo " + nomarchivo + "\n" + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Error. " + ex.getMessage());
         }
-        catch (IOException ioex) {
+        /*catch (IOException ioex) {
             ioex.printStackTrace();
-        }
+        }*/
     }
     
 }
